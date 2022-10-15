470cc49c7147a26ff8643f78c021f29ba7678b52
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-15 21:26:41 +0530

    created server and handled response and requests...

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
-const fs = require('fs');
-const http = require('http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    console.log(res);
-    res.end('<h1>Hii this is Alok</h1>');
-   
-});
+    // Build Filepath dynamic
+    const filePath = path.join(
+        __dirname,
+        "public",
+        res.url === "/" ? "index.html" : req.url
+    );
 
+    // extension of file called
+    let extension = path.extname(filePath);
+
+    // Initial content type
+    let contentType = "text/html";
+
+    // check extension and set content type
+    switch (extension) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/javascript";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
+    }
+
+    //-----Read File-------
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                //  <------'ENOENT' means file not found
+                fs.readFile(path.join(__dirname, 'public', 'errorPage.html'), (err, content) => {
+                    if (err) throw err;
+                    res.writeHead(200, { 'content-type': 'text/html' });
+                    res.end(content, 'utf8');
+
+                });
+
+            }
+            else {
+                //Some server error
+                res.writeHead(500);
+                res.end(`Server Error : ${err.code}`);
+            }
+        }
+
+        // If no error, then load content
+        else {
+            //Success
+            res.writeHead(200, { 'content-type': contentType });
+            res.end(content, 'utf8');
+
+        }
+    });
+});
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, ()=>{console.log(`newly created server is running  at PORT NO: ${PORT}`)});
+server.listen(PORT, () => console.log(`Actual server is running on PORT NO: ${PORT}`));
