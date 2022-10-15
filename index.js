7294308921c0068999de00402a23261bc83893c6
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-15 20:28:20 +0530

    created first server in node js...

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-//console.log('Hello World!');
-
 const fs = require('fs');
+const http = require('http');
 
-fs.writeFileSync('index.html', '! \n Hello World');
+const server = http.createServer((req, res) => {
+    console.log(req);
+    console.log(res);
+    res.end('<h1>Hii this is Alok</h1>');
+   
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, ()=>{console.log(`newly created server is running  at PORT NO: ${PORT}`)});
